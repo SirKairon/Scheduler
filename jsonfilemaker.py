3d6cdead4c2700c237699e6bf2bd8d3a685ab1f5
Mike Jattu <mikejattu@Mikes-MacBook-Air.local>
2023-09-16 15:27:16 -0600

    edited

--- jsonfilemaker.py
+++ jsonfilemaker.py
@@ -1,7 +1,7 @@
 import json
 file = "timetable.json" 
 def jsonfileinput(timetable, file ):
-    with open("timetable.json", "w") as outfile:
+    with open(file, "w") as outfile:
         for day in timetable:
             json_object = json.dump(day,outfile)
     return file
