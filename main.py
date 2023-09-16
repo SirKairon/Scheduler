8d4015629df0ccd0d5737a3746a29b9a2694340e
Sai Kiran (Mike) Jattu <113272168+mikejattu@users.noreply.github.com>
2023-09-16 15:21:25 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# import statements
+import events.py
+#inputing the primary tasks
+pt = str(input("Do you have any primary tasks (y/n)"))
+if  pt == "y":
+    condition = True
+    event 
+    while condition==True:
+        name = 
